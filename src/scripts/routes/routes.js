155830a09695ebdc7e03800cd7e88aaea3c57d44
deinsteins <hidayaatdenii@gmail.com>
2022-06-07 12:56:59 +0700

    Finishing login and register feature

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,7 +1,10 @@
 import Home from "../views/pages/home";
+import LoginRegister from "../views/pages/loginRegister";
 
 const routes = {
-  '/': Home
+  '/': Home,
+  '/home': Home,
+  '/login-register': LoginRegister,
 };
 
 export default routes;

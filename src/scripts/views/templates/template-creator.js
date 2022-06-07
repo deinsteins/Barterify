a5896f257a2fd702deb729a48fd65c5e8af1a2e6
deinsteins <hidayaatdenii@gmail.com>
2022-06-07 12:37:46 +0700

    Create login and register template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -223,7 +223,111 @@ const createProductListTemplate = () => `
               </div>
 `;
 
+const createLoginRegisterFormTemplate = () => `
+            <form class="sign-in-form" id="sign-in-form">
+                <h2 class="title">Log in</h2>
+                <div class="input-field">
+                  <i
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="#ACACAC"
+                      stroke-width="1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </i>
+                  <input type="text" placeholder="Username/Email" id="usernameLogin" required/>
+                </div>
+                <div class="input-field">
+                  <i
+                    ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                      <rect width="24" height="24" fill="none"></rect>
+                      <path
+                        d="M208,80H172V52a44,44,0,0,0-88,0V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80ZM100,52a28,28,0,0,1,56,0V80H100Z"
+                        fill="#acacac"
+                      ></path>
+                    </svg>
+                  </i>
+                  <input type="password" placeholder="Password" id="passwordLogin" required/>
+                </div>
+                <input type="submit" value="Login" class="btn solid" />
+              </form>
+              <form class="sign-up-form" id="sign-up-form">
+                <h2 class="title">Sign up</h2>
+                <div class="input-field">
+                  <i
+                    ><svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="#ACACAC"
+                      stroke-width="1"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </i>
+                  <input type="text" placeholder="Username" id="username" required/>
+                </div>
+                <div class="input-field">
+                  <i
+                    ><svg
+                      style="color: rgb(172, 172, 172)"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-envelope-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"
+                        fill="#acacac"
+                      ></path>
+                    </svg>
+                  </i>
+                  <input type="email" placeholder="Email" id="email" required/>
+                </div>
+                <div class="input-field">
+                  <i
+                    ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                      <rect width="24" height="24" fill="none"></rect>
+                      <path
+                        d="M208,80H172V52a44,44,0,0,0-88,0V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80ZM100,52a28,28,0,0,1,56,0V80H100Z"
+                        fill="#acacac"
+                      ></path>
+                    </svg>
+                  </i>
+                  <input type="password" placeholder="Password" id="password" required minlength="8"/>
+                </div>
+                <div class="input-field">
+                  <i
+                    ><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                      <rect width="24" height="24" fill="none"></rect>
+                      <path
+                        d="M208,80H172V52a44,44,0,0,0-88,0V80H48A16,16,0,0,0,32,96V208a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V96A16,16,0,0,0,208,80ZM100,52a28,28,0,0,1,56,0V80H100Z"
+                        fill="#acacac"
+                      ></path>
+                    </svg>
+                  </i>
+                  <input type="password" placeholder="Password Confirmation" id="repassword" required minlength="8"/>
+                </div>
+                <input type="submit" class="btn" value="Sign up" />
+            </form>
+`;
+
+
 export {
   createFilterFormTemplate,
   createProductListTemplate,
+  createLoginRegisterFormTemplate
 };

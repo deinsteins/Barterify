879f5c701660e71a9e366fd6597bd531eb4cc246
deinsteins <hidayaatdenii@gmail.com>
2022-06-07 13:09:34 +0700

    Create navbar state when the user is logged in and not

--- src/scripts/component/app-bar.js
+++ src/scripts/component/app-bar.js
@@ -1,3 +1,6 @@
+import { createNavlinkWithAuth, createNavlinkWithoutAuth } from "../views/templates/template-creator";
+import BarterifyDbSource from "../data/barterifydb-source";
+
 class AppBar extends HTMLElement {
   connectedCallback() {
     this.render();
@@ -21,29 +24,51 @@ class AppBar extends HTMLElement {
         <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </button>
       <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-          <li>
-            <a href="#" class="block md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page" id="link">Beranda</a>
-          </li>
-          <li>
-            <a href="#" class="block md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" id="link">Informasi</a>
-          </li>
-          <li>
-            <a href="#" class="block md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" id="link">Tentang Kami</a>
-          </li>
-          <li>
-            <a href="#" class="block md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" id="link">Daftar</a>
-          </li>
-          <li>
-            <a href="#" class="block md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" id="link">Masuk</a>
-          </li>
+        <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium" id="navlink">
+         
+          </div>
         </ul>
       </div>
     </div>
   </nav>
     </header>
+    <style>
+      .hide {
+        opacity: 0;
+        scale: 95;
+      }
+      .show {
+        background-color: #112b3c;
+        opacity: 100;
+        scale: 100;
+      }
+      #user a {
+        color: #fff;
+      }
+    </style>
       `;
-  }
+      const navlinkContainer = document.getElementById('navlink');
+      const checkAuth = localStorage.getItem('token');
+      if (checkAuth == null) {
+        navlinkContainer.innerHTML += createNavlinkWithoutAuth();
+      } else {
+        navlinkContainer.innerHTML += createNavlinkWithAuth();
+        document.getElementById('user-menu-button').addEventListener('click', async (e) => {
+          e.preventDefault();
+          const userDropdown = document.getElementById('user');
+          userDropdown.classList.toggle('show');
+        });
+
+        document.getElementById('user-menu-item-2').addEventListener('click', async (e) => {
+          e.preventDefault();
+          const data = await BarterifyDbSource.logout();
+          if (data.success == '') {
+            redirectUserRegister();
+          } 
+        })
+      }
+      
+    }
 }
 
 customElements.define('app-bar', AppBar);

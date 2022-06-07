ac7967b4c9032c4af1400a05b3be250d240fbd65
deinsteins <hidayaatdenii@gmail.com>
2022-06-08 01:29:12 +0700

    Add routes for about us page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,11 +2,13 @@ import Home from "../views/pages/home";
 import LoginRegister from "../views/pages/loginRegister";
 import UserProfile from "../views/pages/userProfile";
 import UserProfileEdit from "../views/pages/userProfileEdit";
+import About from "../views/pages/about";
 
 const routes = {
   '/': Home,
   '/home': Home,
   '/login-register': LoginRegister,
+  '/about': About,
   '/profile': UserProfile,
   '/profile-edit': UserProfileEdit,
 };

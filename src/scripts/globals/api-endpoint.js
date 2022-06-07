0944a3a704f3f86dc8199bca5a2cb914db3f0e6f
deinsteins <hidayaatdenii@gmail.com>
2022-06-07 12:40:47 +0700

    Add base url and API endpoint for login register and logout

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,8 +1,9 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-  REGISTER: `${CONFIG.BASE_URL}auth/register`,
-  SIGNIN: `${CONFIG.BASE_URL}auth/sign_in`,
+  REGISTER: `${CONFIG.BASE_URL}/users/register`,
+  LOGIN: `${CONFIG.BASE_URL}/users/login`,
+  LOGOUT: `${CONFIG.BASE_URL}/users/logout`,
 };
 
 export default API_ENDPOINT;
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: '',
+  BASE_URL: 'http://localhost:3000/api',
   CACHE_NAME: 'Barterify',
 };
 

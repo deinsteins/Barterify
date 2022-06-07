d74b6a3bb6d3a1dd4458b84430c4609a392a628a
deinsteins <hidayaatdenii@gmail.com>
2022-06-08 01:15:07 +0700

    Create profile pages and profile edit form template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -219,6 +219,7 @@ const createProductListTemplate = (product) => `
           </div>
         </a>
         </div>
+
 `;
 
 const createLoginRegisterFormTemplate = () => `
@@ -368,10 +369,233 @@ const createProductDetailTemplate = () => `
     
 `;
 
+const createProfileTemplate = (data) =>   `
+        <div class="mt-11 md:flex no-wrap md:-mx-2" >
+            <!-- Left Side -->
+            <div class="w-full md:w-3/12 md:mx-2" >
+              <!-- Profile Card -->
+              <div class="bg-white p-3 border-t-4 border-green-400">
+                <div class="image overflow-hidden">
+                  <img
+                    class="h-auto w-full mx-auto"
+                    src="images/Logo/user.png"
+                    alt=""
+                  />
+                </div>
+                <h1 class="text-gray-900 font-bold text-xl leading-8 my-1 text-center">${data.data.username}</h1>
+                <ul
+                  class="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm"
+                >
+                  <li class="flex items-center py-3">
+                    <span>Status</span>
+                    <span class="ml-auto"
+                      ><span class="bg-green-500 py-1 px-2 rounded text-white text-sm"
+                        >Active</span
+                      ></span
+                    >
+                  </li>
+                  <li class="flex items-center py-3">
+                    <span>Member since</span>
+                    <span class="ml-auto">Nov 07, 2016</span>
+                  </li>
+                </ul>
+              </div>
+              <!-- End of profile card -->
+              </div>
+              <!-- Right Side -->
+              <div class="w-full md:w-9/12 mx-2 h-64">
+              <!-- Profile tab -->
+              <!-- About Section -->
+              <div class="bg-white p-3 shadow-md rounded-sm">
+                <div
+                  class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
+                >
+                  <span clas="text-green-500">
+                    <svg
+                      class="h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                      />
+                    </svg>
+                  </span>
+                  <span class="tracking-wide">About</span>
+                </div>
+                <div class="text-gray-700 mt-4">
+                  <div class="grid md:grid-cols-2 text-sm mb-3">
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">First Name</div>
+                      <div class="px-4 py-2">${data.data.firstname}</div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">Last Name</div>
+                      <div class="px-4 py-2">${data.data.lastname}</div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">Gender</div>
+                      <div class="px-4 py-2">${data.data.gender}</div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">Contact No.</div>
+                      <div class="px-4 py-2">${data.data.phone}</div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">Current Address</div>
+                      <div class="px-4 py-2">${data.data.address}</div>
+                    </div>
+                    <div class="grid grid-cols-2">
+                      <div class="px-4 py-2 font-semibold">Email.</div>
+                      <div class="px-4 py-2">${data.data.email}</div>
+                    </div>
+                </div>
+                <button
+                class="outline outline-2 outline-green-500 outline-offset-2 block w-full text-sm font-semibold rounded-lg hover:bg-green-300 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4"
+                id="btnEdit">
+                Edit Profile
+              </button>
+              <button
+              id="btnInventory"
+              class="outline outline-2 outline-green-500 outline-offset-2 block w-full text-sm font-semibold rounded-lg hover:bg-green-300 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4"
+            >
+              Inventory saya
+            </button>
+            </div>
+            </div>
+            <!-- End of profile tab -->                
+          </div>
+          </div>
+`;
+
+const createProfileEditFormTemplate = (data) => `
+          <form>
+          <div class="shadow-md overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+              <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="first-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >First name</label
+                    >
+                    <input
+                      type="text"
+                      name="first-name"
+                      id="first-name"
+                      value="${data.data.firstname}"
+                      autocomplete="given-name"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="last-name"
+                      class="block text-sm font-medium text-gray-700"
+                      >Last name</label
+                    >
+                    <input
+                      type="text"
+                      name="last-name"
+                      id="last-name"
+                      value="${data.data.lastname}"
+                      autocomplete="family-name"
+                      class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                      required
+                    />
+                  </div>
+                  <div class="col-span-6 sm:col-span-3">
+                        <label
+                        for="email-address"
+                        class="block text-sm font-medium text-gray-700"
+                        >Email address</label
+                        >
+                        <input
+                        type="text"
+                        name="email-address"
+                        id="email-address"
+                        autocomplete="email"
+                        value="${data.data.email}"
+                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-15 shadow-sm sm:text-sm border-gray-300 rounded-md w-full"
+                        required
+                        disabled
+                        />
+                    </div>
+
+              <div class="col-span-6 sm:col-span-3">
+              <label
+                  for="phone-number"
+                  class="block text-sm font-medium text-gray-700"
+                  >Phone Number</label
+              >
+              <input
+                  type="number"
+                  name="phone-number"
+                  id="phone-number"
+                  autocomplete="phone"
+                  value="${data.data.phone}"
+                  class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  placeholder="Contoh 62888211524416"
+                  required
+              />
+              </div>
+                <div class="col-span-6 sm:col-span-3">
+                  <label
+                    for="gender"
+                    class="block text-sm font-medium text-gray-700"
+                    >Gender</label
+                  >
+                  <select
+                    id="gender"
+                    name="gender"
+                    value="${data.data.gender}"
+                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    required
+                  >
+                    <option>Pria</option>
+                    <option>Wanita</option>
+                  </select>
+                </div>
+                <div class="col-span-6">
+                  <label
+                    for="address"
+                    class="block text-sm font-medium text-gray-700"
+                    >Address</label
+                  >
+                  <textarea
+                    name="saddress"
+                    id="address"
+                    autocomplete="address"
+                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    required
+                  />${data.data.address}</textarea>
+                </div>
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button 
+                type="submit"
+                id="editProfile"
+                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >Save</button>
+            </div>
+          </div>
+          </form>
+`;
+
 export {
   createFilterFormTemplate,
   createProductListTemplate,
   createLoginRegisterFormTemplate,
   createNavlinkWithAuth,
-  createNavlinkWithoutAuth
+  createNavlinkWithoutAuth,
+  createProfileTemplate,
+  createProfileEditFormTemplate,
 };

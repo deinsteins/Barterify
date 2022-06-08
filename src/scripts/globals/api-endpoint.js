6e5721d6f590d1417394d9f14c96a746e0e22656
deinsteins <hidayaatdenii@gmail.com>
2022-06-08 16:03:40 +0700

    Add API endpoint for get all product list

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -5,6 +5,7 @@ const API_ENDPOINT = {
   LOGIN: `${CONFIG.BASE_URL}/users/login`,
   LOGOUT: `${CONFIG.BASE_URL}/users/logout`,
   PROFILE: `${CONFIG.BASE_URL}/profiles`,
+  PRODUCT_LIST: `${CONFIG.BASE_URL}/products/all`,
 };
 
 export default API_ENDPOINT;

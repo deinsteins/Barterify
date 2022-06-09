2fa2e4aa8b9be15f88c9ee0a454e866464702412
deinsteins <hidayaatdenii@gmail.com>
2022-06-09 16:17:00 +0700

    Add routes for user product detail page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -6,6 +6,7 @@ import About from "../views/pages/about";
 import Inventory from "../views/pages/inventory";
 import addProduct from "../views/pages/addProduct";
 import productDetail from "../views/pages/productDetail";
+import userProductDetail from "../views/pages/UserProductDetail";
 
 const routes = {
   '/': Home,
@@ -17,6 +18,7 @@ const routes = {
   '/inventory': Inventory,
   '/product': addProduct,
   '/products/:id': productDetail,
+  '/userproducts/:id': userProductDetail,
 };
 
 export default routes;

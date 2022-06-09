252a8208f76f2946f04731492492b9b4958cf14f
deinsteins <hidayaatdenii@gmail.com>
2022-06-09 14:05:08 +0700

    Add categories and add product form template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -682,10 +682,129 @@ const createTeamCardTemplate = (data) => `
       </div>
 `;
 
+const createAddProductFormTemplate = () => `
+<form action="#" >
+            <div class="shadow-md overflow-hidden sm:rounded-md">
+              <div class="px-4 py-5 bg-white sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-4">
+                    <label
+                    for="product-name"
+                    class="block text-sm font-medium text-gray-700"
+                    >Nama Barang</label
+                    >
+                    <input
+                    type="text"
+                    name="product-name"
+                    id="product-name"
+                    autocomplete="email"
+                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-15 shadow-sm sm:text-sm border-gray-300 rounded-md w-full" required
+                    />
+                </div>
+
+                <div class="col-span-6 sm:col-span-4">
+                <label
+                    for="price"
+                    class="block text-sm font-medium text-gray-700"
+                    >Harga Jual</label
+                >
+                <input
+                    type="number"
+                    name="price"
+                    id="price"
+                    autocomplete="phone"
+                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    required 
+                />
+                </div>
+                  <div class="col-span-6 sm:col-span-3">
+                    <label
+                      for="product-category"
+                      class="block text-sm font-medium text-gray-700"
+                      >Kategori</label
+                    >
+                    <select
+                      id="product-category"
+                      name="product-category"
+                      autocomplete="product-category-name"
+                      class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                    >
+                    </select>
+                  </div>
+
+                  <div class="col-span-6 sm:col-span-4">
+                <label
+                    for="date"
+                    class="block text-sm font-medium text-gray-700"
+                    >Tanggal Beli</label
+                >
+                <input
+                    type="date"
+                    name="date"
+                    id="date"
+                    autocomplete="date"
+                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                    required 
+                />
+                </div>
+
+                  <div class="col-span-6">
+                  <label
+                    class="block text-sm font-medium text-gray-700"
+                    for="product-image"
+                    >Upload foto</label
+                > <input type="file" name="product-image"
+                id="product-image" class="w-full text-gray-700 px-3 py-2 border rounded" >
+                </div>
+
+                  <div class="col-span-6">
+                    <label
+                      for="description"
+                      class="block text-sm font-medium text-gray-700"
+                      >Deskripsi</label
+                    >
+                    <textarea name="description"
+                    id="description" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" rows="4"></textarea>
+                
+                  </div>
+
+                  <div class="col-span-6">
+                  <label
+                    for="location"
+                    class="block text-sm font-medium text-gray-700"
+                    >Lokasi</label
+                  >
+                  <textarea name="location"
+                  id="location" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" rows="4"></textarea>
+              
+                </div>
+                </div>
+              </div>
+              <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <button
+                  id="btnSubmit"
+                  type="submit"
+                  class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+`;
+
+const createCategoriesTemplate = (category) =>`
+      <div>
+      <option value="${category.id}">${category.name}</option>
+      </div>
+`;
+
 export {
   createFilterFormTemplate,
   createProductListTemplate,
   createUserProductListTemplate,
+  createAddProductFormTemplate,
+  createCategoriesTemplate,
   createProductDetailTemplate,
   createLoginRegisterFormTemplate,
   createNavlinkWithAuth,

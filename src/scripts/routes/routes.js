ba262af41d7b29ed853baf0dae3b6dc5ae243b37
deinsteins <hidayaatdenii@gmail.com>
2022-06-23 00:07:17 +0700

    Add routes for public chat page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,15 +1,16 @@
-import Home from "../views/pages/home";
-import LoginRegister from "../views/pages/loginRegister";
-import UserProfile from "../views/pages/userProfile";
-import UserProfileEdit from "../views/pages/userProfileEdit";
-import About from "../views/pages/about";
-import InformationPage from "../views/pages/informationPage";
-import Inventory from "../views/pages/inventory";
-import addProduct from "../views/pages/addProduct";
-import productDetail from "../views/pages/productDetail";
-import userProductDetail from "../views/pages/UserProductDetail";
-import UserProductEdit from "../views/pages/userProductEdit";
-import Favorites from "../views/pages/favorite";
+import Home from '../views/pages/home';
+import LoginRegister from '../views/pages/loginRegister';
+import UserProfile from '../views/pages/userProfile';
+import UserProfileEdit from '../views/pages/userProfileEdit';
+import About from '../views/pages/about';
+import InformationPage from '../views/pages/informationPage';
+import Inventory from '../views/pages/inventory';
+import addProduct from '../views/pages/addProduct';
+import productDetail from '../views/pages/productDetail';
+import userProductDetail from '../views/pages/userProductDetail';
+import UserProductEdit from '../views/pages/userProductEdit';
+import Favorites from '../views/pages/favorite';
+import Chat from '../views/pages/chat';
 
 const routes = {
   '/': Home,
@@ -23,8 +24,9 @@ const routes = {
   '/product': addProduct,
   '/products/:id': productDetail,
   '/userproducts/:id': userProductDetail,
-  '/product-edit/:id' : UserProductEdit,
+  '/product-edit/:id': UserProductEdit,
   '/wishlist': Favorites,
+  '/chat': Chat,
 };
 
 export default routes;

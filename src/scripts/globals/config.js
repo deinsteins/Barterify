c84a33dee980bc78a2192e865499ada0401a6a9b
deinsteins <hidayaatdenii@gmail.com>
2022-06-30 03:05:21 +0700

    Change Base Url API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  BASE_URL: 'https://barterify-api.herokuapp.com',
+  BASE_URL: 'https://barterify-api.herokuapp.com/api',
   BASE_IMAGE_URL: 'https://barterify-api.herokuapp.com/',
   CACHE_NAME: 'Barterify',
   DATABASE_NAME: 'barterify-database',
